7c71d2c4710c8ab8c3a354794e073be4e6edb54f
mahmud987 <safemahmud987@gmail.com>
2022-10-12 00:55:32 +0600

    readme file edited

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBrain } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
         <div className='flex flex-col md:flex-row justify-between p-5  bg-emerald-300 rounded-md'>
-            <h1 className='text-4xl font-serif font-bold text-orange-700'>Cheak your Intilligence</h1>
+            <h1 className='text-4xl font-serif font-bold text-orange-700'>  <FontAwesomeIcon icon={faBrain} />    Cheak your Intilligence</h1>
             <div className='flex items-center mr-4'>
                 <Link className='font-bold text-cyan-700 m-4' to={'/'} >Home</Link>
                 <Link className='font-bold text-cyan-700 m-4' to={'/statistics'} >Statistics</Link>
