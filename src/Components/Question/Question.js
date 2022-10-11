d2319979e8f7006c21fd2835b742e789f08b2764
mahmud987 <safemahmud987@gmail.com>
2022-10-12 00:34:09 +0600

    eye icon add

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -4,11 +4,14 @@ import parse from 'html-react-parser';
 import Option from '../Options/Option';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 const Question = ({ questionss, rightAnswer, wrongAnswer }) => {
     const { question, options, correctAnswer } = questionss;
 
     const notify1 = () => toast("Your answer is right !");
     const notify2 = () => toast("Your answer is wrong !");
+    const notify3 = (item) => toast(item);
 
 
     const answer = (ans) => {
@@ -23,12 +26,16 @@ const Question = ({ questionss, rightAnswer, wrongAnswer }) => {
 
         }
     }
-
+    console.log(correctAnswer)
     return (
-        <div className='mt-12'>
+        <div className='mt-12 border-4 border-black relative'>
 
-            <h1 className='text-2xl font-bold text-start m-3 '>##{parse(question)}</h1>
-            <div className='border-4 border-black p-3'>
+            <button onClick={() => notify3(`${correctAnswer}`)} className='absolute top-0 right-0'>
+                <FontAwesomeIcon icon={faEye} />
+            </button>
+
+            <h1 className='text-2xl font-bold text-start m-3 '>{parse(question)}</h1>
+            <div className=' p-3'>
                 <h1 className='font-bold text-start m-2'>Choose the correct answer</h1>
 
                 <div className=' grid grid-cols-1 md:grid-cols-2 gap-3'>
@@ -37,7 +44,7 @@ const Question = ({ questionss, rightAnswer, wrongAnswer }) => {
                     }
                 </div>
             </div>
-            <div className='text-4xl'>
+            <div className='text-3xl'>
                 <ToastContainer
                     position="top-center"
                     autoClose={2000}
